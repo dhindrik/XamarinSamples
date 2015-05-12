a92ced3f425c502b1e8d5478e97230346a0f1d69
Daniel Hindrikes <daniel@hindrikes.se>
2015-05-12 10:10:11 +0200

    ioc helper

---
+++ Drivis/Drivis.Core/IoC/AutofacResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace Drivis.Core.IoC
+{
+    public class AutofacResolver : IResolver
+    {
+        private readonly IContainer _container;
+
+        public AutofacResolver(IContainer container)
+        {
+            _container = container;
+        }
+
+        public T Resolve<T>()
+        {
+            _container.Resolve<T>();
+        }
+    }
+}
---
+++ Drivis/Drivis.Core/IoC/Resolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drivis.Core.IoC
+{
+    public class Resolver
+    {
+        private static IResolver _resolver;
+
+        public static void SetResolver(IResolver resolver)
+        {
+            _resolver = resolver;
+        }
+
+        public static T Resolve<T>()
+        {
+            if (_resolver == null)
+            {
+                throw new Exception("You have to set resolver before use this method");
+            }
+
+            return _resolver.Resolve<T>();
+        }
+    }
+}
