a350a165c1f961b10076a984a705279b7edcdbc3
Daniel Hindrikes <daniel@hindrikes.se>
2015-05-12 10:16:08 +0200

    created restclient

---
+++ Drivis/Drivis.Core/Networking/RestClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Drivis.Core.Networking
+{
+    public class RestClient : IRestClient
+    {
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            var client = new HttpClient();
+
+            var json = await client.GetStringAsync(uri);
+
+            var result = JsonConvert.DeserializeObject<T>(json);
+
+            return result;
+        }
+    }
+}
