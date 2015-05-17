c6e1db6d6136a8ef81dbb6f261a165486accc0d6
Daniel Hindrikes <daniel@hindrikes.se>
2015-05-17 16:17:44 +0200

    ui test

--- Drivis/UITests/AppInitializer.cs
+++ Drivis/UITests/AppInitializer.cs
@@ -5,23 +5,17 @@ using System.Reflection;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
-namespace UITests
+namespace Drivis.UITests
 {
     public class AppInitializer
     {
         public static IApp StartApp(Platform platform)
         {
+
             if (platform == Platform.Android)
             {
-                var path = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-                var info = new FileInfo(path);
-                var directory = info.Directory.Parent.Parent.Parent.FullName;
-
-                var pathToAPK = Path.Combine(directory, "Drivis.XForms\\Drivis.XForms.Droid", "bin", "Debug", "Drivis.XForms.Droid.apk");
-
                 return ConfigureApp
-                    .Android.ApkFile(pathToAPK)
-                    .StartApp();
+                    .Android.StartApp();
             }
 
             return ConfigureApp
--- Drivis/UITests/Tests.cs
+++ Drivis/UITests/Tests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
-namespace UITests
+namespace Drivis.UITests
 {
     [TestFixture(Platform.Android)]
     //[TestFixture(Platform.iOS)]
